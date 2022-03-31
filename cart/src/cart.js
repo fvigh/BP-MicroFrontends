d74cf9e0c785844de4102f0a1e6274bf7972a58a
fvigh <43516626+fvigh@users.noreply.github.com>
2022-03-31 23:39:32 +0200

    updated server url

--- cart/src/cart.js
+++ cart/src/cart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BehaviorSubject } from "rxjs";
 
-const API_SERVER = "http://localhost:8080";
+const API_SERVER = "https://static-bp-mf.vercel.app";
 
 export const jwt = new BehaviorSubject(null);
 export const cart = new BehaviorSubject(null);
--- restaurants/src/restaurants.js
+++ restaurants/src/restaurants.js
@@ -1,4 +1,4 @@
-const API_SERVER = "http://localhost:8080";
+const API_SERVER = "https://static-bp-mf.vercel.app";
 
 export const getRestaurants = () =>
   fetch(`${API_SERVER}/restaurants`).then((res) => res.json());
